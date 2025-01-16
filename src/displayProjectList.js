640a8a0ac58104109547a9d86f045e2c7e69f9da
Gabriel Mello <gabrielpdmello@gmail.com>
2025-01-15 21:44:03 -0300

    Import new modules

--- src/displayProjectList.js
+++ src/displayProjectList.js
@@ -3,6 +3,8 @@ import { displayAllProjects } from "./displayAllProjects.js";
 import { newProjectMenu } from "./newProjectMenu.js";
 import { removeAllChild } from "./removeAllChild.js";
 import { displayProject } from "./displayProject.js";
+import { editProject } from "./editProject.js";
+import { removeProject } from "./removeProject.js";
 
 function displayProjectList() {
     const projectListElement = document.querySelector(".projects-list");
@@ -28,9 +30,29 @@ function displayProjectList() {
     });
     projectListElement.appendChild(allTasks)
 
-    projectList.forEach(project => {
+    projectList.forEach((project, index) => {
         const item = document.createElement("li");
-        item.textContent = project.title;
+        item.classList.add("project-item");
+        const projectTitle = document.createElement("span");
+        projectTitle.textContent = project.title;
+        const editIcon = document.createElement("span");
+        editIcon.classList.add("edit-icon");
+
+        editIcon.addEventListener("click", ()=> {
+            editProject(project, editIcon);
+        });
+        
+        const deleteIcon = document.createElement("span");
+        deleteIcon.classList.add("delete-icon");
+
+        deleteIcon.addEventListener("click", ()=> {
+            removeProject(project, index, deleteIcon);
+        })
+        
+        item.appendChild(projectTitle);
+        item.appendChild(editIcon);
+        item.appendChild(deleteIcon);
+        
         item.addEventListener("click", () => {
             displayProject(project, taskContainer);
         });
