e7c57ed6965b99ac7e104661cd62d13832b9c263
Gabriel Mello <gabrielpdmello@gmail.com>
2025-01-15 21:43:28 -0300

    Add module to remove project

---
+++ src/removeProject.js
@@ -0,0 +1,43 @@
+import { displayAllProjects } from "./displayAllProjects";
+import { displayProjectList } from "./displayProjectList";
+import { projectList } from "./projectList";
+import { removeAllChild } from "./removeAllChild";
+
+function removeProject(project, projectIndex, button) {
+    const window = document.querySelector(".window");
+    removeAllChild(window);
+
+    const heading = document.createElement("h2");
+    heading.classList.add("window-heading");
+    heading.textContent = "Remove project";
+    const removeProjectText = document.createElement("p");
+    removeProjectText.textContent = `Do you want to remove project ${project.title}?`;
+    const removeProjectButton = document.createElement("button");
+    removeProjectButton.textContent = "Yes";
+    const cancelProjectButton = document.createElement("button");
+    cancelProjectButton.textContent = "No";
+
+    removeProjectButton.addEventListener("click", ()=> {
+        projectList.splice(projectIndex, 1);
+        displayAllProjects();
+        displayProjectList();
+        window.classList.add("hide")
+    })
+
+    cancelProjectButton.addEventListener("click", ()=> {
+        window.classList.add("hide");
+    })
+
+    window.appendChild(heading);
+    window.appendChild(removeProjectText);
+    window.appendChild(cancelProjectButton);
+    window.appendChild(removeProjectButton);
+
+    document.addEventListener("click", e => {
+        if (button.contains(e.target) && window.classList.contains("hide")) {
+          window.classList.remove("hide");
+        }
+    });
+}
+
+export {removeProject};
