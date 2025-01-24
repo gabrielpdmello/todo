07214a625778854c7e9e1f84751eef8e35f242c1
Gabriel Mello <gabrielpdmello@gmail.com>
2025-01-24 13:07:34 -0300

    Add container to buttons

--- src/displayRemoveProjectMenu.js
+++ src/displayRemoveProjectMenu.js
@@ -15,36 +15,37 @@ function displayRemoveProjectMenu(project, button) {
     const removeProjectText = document.createElement("p");
     removeProjectText.classList.add("window-text");
     removeProjectText.textContent = `Do you want to remove project ${project.title}?`;
-    const removeProjectButton = document.createElement("button");
-    removeProjectButton.classList.add("done-button");
-    removeProjectButton.classList.add("button--primary");
-    removeProjectButton.textContent = "Yes";
-    const cancelProjectButton = document.createElement("button");
-    cancelProjectButton.classList.add("done-button");
-    cancelProjectButton.classList.add("button--primary");
-    cancelProjectButton.textContent = "No";
+    const buttonContainer = document.createElement("span");
+    buttonContainer.classList.add("window-button-container");
+    const removeButton = document.createElement("button");
+    removeButton.classList.add("button--primary");
+    removeButton.textContent = "Yes";
+    const cancelButton = document.createElement("button");
+    cancelButton.classList.add("button--primary");
+    cancelButton.textContent = "No";
 
-    removeProjectButton.addEventListener("click", ()=> {
+    removeButton.addEventListener("click", ()=> {
         projectList.splice(projectIndex, 1);
         display.allProjects();
         display.projectList();
         window.classList.add("hide")
     })
 
-    cancelProjectButton.addEventListener("click", ()=> {
+    cancelButton.addEventListener("click", ()=> {
         window.classList.add("hide");
-        cancelProjectButton.focus();
     })
 
     windowForm.appendChild(heading);
     windowForm.appendChild(removeProjectText);
-    windowForm.appendChild(cancelProjectButton);
-    windowForm.appendChild(removeProjectButton);
+    buttonContainer.appendChild(cancelButton);
+    buttonContainer.appendChild(removeButton);
+    windowForm.appendChild(buttonContainer);
     window.appendChild(windowForm);
 
     document.addEventListener("click", e => {
         if (button.contains(e.target) && window.classList.contains("hide")) {
           window.classList.remove("hide");
+          cancelButton.focus();
         }
     });
 }
