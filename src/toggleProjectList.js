88ca19a77b1a5cafc0328ab2a77f2ece01250e82
Gabriel Mello <gabrielpdmello@gmail.com>
2025-02-17 19:20:13 -0300

    Add transitions

--- src/toggleProjectList.js
+++ src/toggleProjectList.js
@@ -4,9 +4,6 @@ const toggleProjectList = function() {
     const taskContainer = document.querySelector(".task-container");
 
     let jmediaquery = window.matchMedia("(max-width: 50em)");
-    if (jmediaquery.matches) {
-        projectListContainer.classList.add("hide");
-    }
 
     window.addEventListener("resize", () => {
         if (!jmediaquery.matches) {
@@ -21,16 +18,16 @@ const toggleProjectList = function() {
         if (jmediaquery.matches) {
             if (menuButton.contains(e.target) && projectListContainer.classList.contains("project-list-show")) {
                 projectListContainer.classList.remove("project-list-show");
-                taskContainer.classList.remove("hide");
             } else if (menuButton.contains(e.target) && !projectListContainer.classList.contains("project-list-show")) {
                 projectListContainer.classList.add("project-list-show");
-                taskContainer.classList.add("hide");
             }
         } else if (!jmediaquery.matches) {
-            if (menuButton.contains(e.target) && projectListContainer.classList.contains("hide")) {
-                projectListContainer.classList.remove("hide");
-            } else if (menuButton.contains(e.target) && !projectListContainer.classList.contains("hide")) {
-                projectListContainer.classList.add("hide");
+            if (menuButton.contains(e.target) && projectListContainer.classList.contains("project-list-hide")) {
+                projectListContainer.classList.remove("project-list-hide");
+                taskContainer.classList.remove("task-container-max-width");
+            } else if (menuButton.contains(e.target) && !projectListContainer.classList.contains("project-list-hide")) {
+                projectListContainer.classList.add("project-list-hide");
+                taskContainer.classList.add("task-container-max-width");
             }
         }
     });
